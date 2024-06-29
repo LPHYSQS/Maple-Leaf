394d30955a43264b45adc427d8c92c3e5e2c33d7
已逝情殇 <98212099+LPHYSQS@users.noreply.github.com>
2024-06-29 15:48:44 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,29 +9,46 @@ document.addEventListener('DOMContentLoaded', function() {
     var downloadZipButton = document.getElementById('downloadZipButton');
     var versionText = document.getElementById('versionText');
     var rightImageContainer = document.getElementById('rightImageContainer');
-    var socialButtons=document.getElementById('socialButtons');
-    var yearVersionText=document.getElementById('yearVersionText');
+    var socialButtons = document.getElementById('socialButtons');
+    var yearVersionText = document.getElementById('yearVersionText');
+
+    // Function to check screen width and toggle rightImageContainer visibility and studioText font size
+    function checkScreenWidth() {
+        if (window.matchMedia("(max-width: 956px)").matches) {
+            rightImageContainer.style.display = 'none';
+            studioText.style.fontSize = 'smaller'; // Adjust the font size as needed
+        } else {
+            rightImageContainer.style.display = 'block';
+            studioText.style.fontSize = ''; // Reset to default font size
+        }
+    }
+
+    // Check screen width on initial load
+    checkScreenWidth();
+
+    // Add event listener for screen size changes
+    window.addEventListener('resize', checkScreenWidth);
 
     studioLogo.onload = function() {
         studioLogoContainer.style.opacity = '1';
 
         setTimeout(function() {
             studioLogoContainer.style.opacity = '0';
-            
+
             setTimeout(function() {
                 studioLogoContainer.style.display = 'none';
                 mainContent.style.display = 'flex';
-                rightImageContainer.style.display = 'block'; // 添加这一行
                 socialButtons.style.display = 'flex';
+                checkScreenWidth(); // Check screen width again after display change
                 setTimeout(function() {
                     card.style.opacity = '1';
                     title.style.opacity = '1';
                     downloadExeButton.style.opacity = '1';
                     downloadZipButton.style.opacity = '1';
                     versionText.style.opacity = '1';
-                    rightImageContainer.style.opacity = '1'; // 添加这一行
-                    socialButtons.style.opacity = '1'; // 添加这一行
-                    yearVersionText.style.opacity = '1'; // 添加这一行
+                    rightImageContainer.style.opacity = '1'; // Add this line
+                    socialButtons.style.opacity = '1'; // Add this line
+                    yearVersionText.style.opacity = '1'; // Add this line
                 }, 100);
             }, 1000);
         }, 2000);
